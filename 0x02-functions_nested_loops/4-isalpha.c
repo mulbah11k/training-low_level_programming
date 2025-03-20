d71f794f591c0380853fd766732a9a5f83cb6a6c
mulbah11k <mulbahtkolleh@gmail.com>
2025-03-20 18:09:16 +0000

    70% done

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+int _isalpha(int c)
+{
+    if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+    {
+        return (1);
+    }
+    return (0);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+int print_last_digit(int n)
+{
+    int last_digit;
+
+    last_digit = n % 10;
+    if (last_digit < 0)
+    {
+        last_digit = -last_digit;
+    }
+    _putchar('0' + last_digit);
+    return last_digit;
+}
