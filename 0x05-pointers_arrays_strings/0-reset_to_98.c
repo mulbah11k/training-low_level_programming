1fc14254adc44e241d24c664a75e333fe6c49026
mulbah11k <mulbahtkolleh@gmail.com>
2025-04-29 10:32:55 +0000

    Implement a function that update a pointer to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - take a pointer and update it
+ *
+ * @n: value to be update
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
