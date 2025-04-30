b37baba2212068531786fc60c7adaf1721d91365
mulbah11k <mulbahtkolleh@gmail.com>
2025-04-30 06:53:14 +0000

    Implement a function that swaps a value of two intergers.

--- 0x05-pointers_arrays_strings/10-atoi.c
+++ 0x05-pointers_arrays_strings/10-atoi.c
@@ -2,15 +2,32 @@
 #include <stdio.h>
 
 /**
- * main - check the code
+ * _atoi - Converts a string to an integer
+ * @s: The string to convert
  *
- * Return: Always 0.
+ * Return: The integer value
  */
 int _atoi(char *s)
 {
-	check = 0;
-	s
-	while(s[check] != '\0')
-	{
+	int i = 0;
+	int sign = 1;
+	int result = 0;
+	int started = 0;
 
-	}
+    while (s[i])
+    {
+        if (s[i] == '-')
+            sign *= -1;
+        else if (s[i] >= '0' && s[i] <= '9')
+        {
+            result = result * 10 + (s[i] - '0');
+            started = 1;
+        }
+        else if (started)
+            break;
+        i++;
+    }
+
+    return sign * result;
+}
+
